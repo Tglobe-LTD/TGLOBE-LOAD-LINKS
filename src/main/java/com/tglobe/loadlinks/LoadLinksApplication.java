2e11544b31631323de1ebf9fe269d6dac159de9c
bright <brightliontech@gmail.com>
2026-03-17 11:19:55 +0100

    Fix: Add SpringBootServletInitializer for WAR deployment

--- src/main/java/com/tglobe/loadlinks/LoadLinksApplication.java
+++ src/main/java/com/tglobe/loadlinks/LoadLinksApplication.java
@@ -2,10 +2,17 @@ package com.tglobe.loadlinks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LoadLinksApplication {
-
+public class LoadLinksApplication extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LoadLinksApplication.class);
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(LoadLinksApplication.class, args);
     }
